4c86d4f4e33c63397a4317c75d9809f560965131
Marcin Szmagara <mszmagara@internships.antmicro.com>
2020-10-01 15:11:02 +0200

    add vertical lines to plot

--- vot_report_generator/policy_test.py
+++ vot_report_generator/policy_test.py
@@ -11,11 +11,33 @@ import cv2
 
 from generate_images import generate_images
 
+def add_images_under(ax, input_sequence):
+    fig = ax.get_figure()
+    width, height = map(int, fig.get_size_inches() * fig.dpi)
+
+    annotationsHeight = 0.08
+    fig.subplots_adjust(bottom=annotationsHeight + 0.08, top=1.0)
+
+    imgHeight = int(height * annotationsHeight)
+    imgShape = image.imread(input_sequence[0]).shape
+    imgAspectRatio = imgShape[1] / imgShape [0]
+    imgWidth = int(imgAspectRatio * imgHeight)
+    spaceSize = 0.07
+    spacedImageSize = int(imgWidth * (1.0 + spaceSize))
+    nImages = int(width * 0.70) // spacedImageSize
+    for it in range(nImages):
+        imgIdx = len(input_sequence) // nImages * it
+        ax.axvline(x=imgIdx, linewidth=0.25, color='red')
+        img = image.imread(input_sequence[imgIdx])
+        img = cv2.resize(img, dsize=(imgWidth, imgHeight))
+        ax.get_figure().figimage(img, int(width * 0.1375) + it * spacedImageSize, 0)
+
+
 def ious_combined_graph(test_results, input_sequence):
     ious = pd.concat([test['iou'] for test in test_results],
             axis=1, join='inner')
     mean = ious.mean(axis=1)
-    ax = ious.plot(legend=False, grid=True, title='Accuracy for '
+    ax = ious.plot(legend=False, grid=False, title='Accuracy for '
             + 'every pass and their mean')
     for line in ax.lines:
         line.set_color('#C0C0C0')
@@ -28,27 +50,12 @@ def ious_combined_graph(test_results, input_sequence):
                           markersize=15, label='Mean (smoothed)'),
                           mlines.Line2D([], [], color='#C0C0C0',
                           markersize=15, label='Raw')])
-    ax.grid(color='red', lw = 0.25)
+    ax.grid(color='white', lw = 0.75)
 
     fig = ax.get_figure()
-    width, height = map(int, fig.get_size_inches() * fig.dpi)
-
-    annotationsHeight = 0.15
-    fig.subplots_adjust(bottom=annotationsHeight + 0.08, top=1.0)
-
-    imgHeight = int(height * annotationsHeight)
-    imgShape = image.imread(input_sequence[0]).shape
-    imgAspectRatio = imgShape[1] / imgShape [0]
-    imgWidth = int(imgAspectRatio * imgHeight)
-    spaceSize = 0.1
-    spacedImageSize = int(imgWidth * (1.0 + spaceSize))
-    nImages = max(width - 2 * spacedImageSize, 0) // spacedImageSize
-    for it in range(nImages):
-        img = image.imread(input_sequence[len(input_sequence) // nImages * it])
-        img = cv2.resize(img, dsize=(imgWidth, imgHeight))
-        ax.get_figure().figimage(img, width // 8 + it * spacedImageSize, 0)
-
-    return {'ious_combined.png': ax.get_figure()}
+    add_images_under(ax, input_sequence)
+    
+    return {'ious_combined.png': fig}
 
 def generate(name, test, link, test_results, tests_input_path):
     env = Environment(
